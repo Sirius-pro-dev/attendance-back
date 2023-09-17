ef5bfe643259ded331b1404321c3643adb36cd9a
Naziser <you@example.com>
2023-09-17 18:49:33 +0300

    fix: deleting unnecessary comment

--- src/connect.ts
+++ src/connect.ts
@@ -3,11 +3,6 @@ import mongoose from 'mongoose';
 export const connect = () => {
     mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-    // const Cat = mongoose.model('Cat', new mongoose.Schema({ name: String }));
-    
-    // const kitty = new Cat({ name: 'Zildjian' });
-    // kitty.save().then(() => console.log('meow'));
-
     return async function() {
         await mongoose.disconnect();
     }

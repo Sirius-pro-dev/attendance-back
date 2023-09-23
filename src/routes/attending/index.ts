d7e19d548acd2fa931ae4d8f347efcff34241359
Daniel Usov <dan.usov82@gmail.com>
2023-09-23 17:52:02 +0300

    feat: return error

--- src/routes/attending/index.ts
+++ src/routes/attending/index.ts
@@ -1,44 +1,60 @@
 export default async function (fastify) {
   fastify.post('/', (request, reply) => {
-    if (!request.isAuthenticated) {
-      reply.status(401).send({ error: 'Unauthorized' });
-      return;
-    }
-    const { sessionID, studentID } = request.body;
+    try {
+      if (!request.isAuthenticated) {
+        reply.status(401).send({ error: 'Unauthorized' });
+        return;
+      }
+      const { sessionID, studentID } = request.body;
 
-    reply.status(201).send({});
+      reply.status(201).send({});
+    } catch (error) {
+      reply.status(500).send({ error: 'Internal Server Error' });
+    }
   });
   fastify.get('/:attendingId', (request, reply) => {
-    if (!request.isAuthenticated) {
-      reply.status(401).send({ error: 'Unauthorized' });
-      return;
-    }
-    const attendingId = request.params.attendingId;
+    try {
+      if (!request.isAuthenticated) {
+        reply.status(401).send({ error: 'Unauthorized' });
+        return;
+      }
+      const attendingId = request.params.attendingId;
 
-    reply.status(200).send({});
+      reply.status(200).send({});
+    } catch (error) {
+      reply.status(500).send({ error: 'Internal Server Error' });
+    }
   });
   fastify.put('/:attendingId', (request, reply) => {
-    const nameIsAlreadyInUse = false;
-    if (!request.isAuthenticated) {
-      reply.status(401).send({ error: 'Unauthorized' });
-      return;
-    }
-    const attendingId = request.params.attendingId;
-    const { sessionID, studentID } = request.body;
+    try {
+      const nameIsAlreadyInUse = false;
+      if (!request.isAuthenticated) {
+        reply.status(401).send({ error: 'Unauthorized' });
+        return;
+      }
+      const attendingId = request.params.attendingId;
+      const { sessionID, studentID } = request.body;
 
-    if (nameIsAlreadyInUse) {
-      reply.status(409).send({ error: 'Name is already in use' });
-    }
+      if (nameIsAlreadyInUse) {
+        reply.status(409).send({ error: 'Name is already in use' });
+      }
 
-    reply.status(200).send({});
+      reply.status(200).send({});
+    } catch (error) {
+      reply.status(500).send({ error: 'Internal Server Error' });
+    }
   });
   fastify.delete('/:attendingId', (request, reply) => {
-    if (!request.isAuthenticated) {
-      reply.status(401).send({ error: 'Unauthorized' });
-      return;
-    }
-    const attendingId = request.params.attendingId;
+    try {
+      if (!request.isAuthenticated) {
+        reply.status(401).send({ error: 'Unauthorized' });
+        return;
+      }
+      const attendingId = request.params.attendingId;
 
-    reply.status(204).send({});
+      reply.status(204).send({});
+    } catch (error) {
+      reply.status(500).send({ error: 'Internal Server Error' });
+    }
   });
 }
--- src/routes/sessions/index.ts
+++ src/routes/sessions/index.ts
@@ -1,10 +1,14 @@
 export default async function (fastify) {
   fastify.get('/', (request, reply) => {
-    if (!request.isAuthenticated) {
-      reply.status(401).send({ error: 'Unauthorized' });
-      return;
-    }
+    try {
+      if (!request.isAuthenticated) {
+        reply.status(401).send({ error: 'Unauthorized' });
+        return;
+      }
 
-    reply.status(200).send([{}]);
+      reply.status(200).send([{}]);
+    } catch (error) {
+      reply.status(500).send({ error: 'Internal Server Error' });
+    }
   });
 }

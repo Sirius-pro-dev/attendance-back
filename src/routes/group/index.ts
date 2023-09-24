c54cf0e60c9b4bf16cbe0298100d48ab4544b639
Daniel <91885191+DanielUsov@users.noreply.github.com>
2023-09-24 17:21:48 +0300

    feat: return error (#6)
    Co-authored-by: Daniel Usov <dan.usov82@gmail.com>

--- src/routes/group/index.ts
+++ src/routes/group/index.ts
@@ -1,44 +1,60 @@
 export default async function (fastify) {
   fastify.post('/', (request, reply) => {
-    if (!request.isAuthenticated) {
-      reply.status(401).send({ error: 'Unauthorized' });
-      return;
-    }
-    const { title } = request.body;
+    try {
+      if (!request.isAuthenticated) {
+        reply.status(401).send({ error: 'Unauthorized' });
+        return;
+      }
+      const { title } = request.body;
 
-    reply.status(201).send({});
+      reply.status(201).send({});
+    } catch (error) {
+      reply.status(500).send({ error: 'Internal Server Error' });
+    }
   });
   fastify.get('/:groupId', (request, reply) => {
-    if (!request.isAuthenticated) {
-      reply.status(401).send({ error: 'Unauthorized' });
-      return;
-    }
-    const groupId = request.params.groupId;
+    try {
+      if (!request.isAuthenticated) {
+        reply.status(401).send({ error: 'Unauthorized' });
+        return;
+      }
+      const groupId = request.params.groupId;
 
-    reply.status(200).send({});
+      reply.status(200).send({});
+    } catch (error) {
+      reply.status(500).send({ error: 'Internal Server Error' });
+    }
   });
   fastify.put('/:groupId', (request, reply) => {
-    const nameIsAlreadyInUse = false;
-    if (!request.isAuthenticated) {
-      reply.status(401).send({ error: 'Unauthorized' });
-      return;
-    }
-    const groupId = request.params.groupId;
-    const { title } = request.body;
+    try {
+      const nameIsAlreadyInUse = false;
+      if (!request.isAuthenticated) {
+        reply.status(401).send({ error: 'Unauthorized' });
+        return;
+      }
+      const groupId = request.params.groupId;
+      const { title } = request.body;
 
-    if (nameIsAlreadyInUse) {
-      reply.status(409).send({ error: 'Name is already in use' });
-    }
+      if (nameIsAlreadyInUse) {
+        reply.status(409).send({ error: 'Name is already in use' });
+      }
 
-    reply.status(200).send({});
+      reply.status(200).send({});
+    } catch (error) {
+      reply.status(500).send({ error: 'Internal Server Error' });
+    }
   });
   fastify.delete('/:groupId', (request, reply) => {
-    if (!request.isAuthenticated) {
-      reply.status(401).send({ error: 'Unauthorized' });
-      return;
-    }
-    const groupId = request.params.groupId;
+    try {
+      if (!request.isAuthenticated) {
+        reply.status(401).send({ error: 'Unauthorized' });
+        return;
+      }
+      const groupId = request.params.groupId;
 
-    reply.status(204).send({});
+      reply.status(204).send({});
+    } catch (error) {
+      reply.status(500).send({ error: 'Internal Server Error' });
+    }
   });
 }

0adaca356f5b2ee3393e444ef9dbff2264c33594
Daniel <dan.usov82@gmail.com>
2023-09-28 22:09:55 +0300

    add: seed chack

--- src/connect.ts
+++ src/connect.ts
@@ -1,11 +1,8 @@
 import mongoose from 'mongoose';
+import { mongoConfig } from './configs/mongo';
 
 export const connect = async fastify => {
-  await mongoose.connect(
-    `mongodb://${process.env.SIRIUS_X_MONGO_USERNAME}:${process.env.SIRIUS_X_MONGO_PASSWORD}@${
-      process.env.SIRIUS_X_DB
-    }:27017/${process.env.SIRIUS_X_MONGO_AUTHSOURCE ? `?authSource=${process.env.SIRIUS_X_MONGO_AUTHSOURCE}` : ''}`
-  );
+  await mongoose.connect(mongoConfig.connectURL);
 
   fastify.log.info('Connected to db');
 

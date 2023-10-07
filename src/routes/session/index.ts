dabd2701aa2b9b219bb981f884b08f34d6a00d25
Daniel Usov <dan.usov82@gmail.com>
2023-10-07 10:13:22 +0300

    feat: qr gen

--- src/routes/session/index.ts
+++ src/routes/session/index.ts
@@ -1,3 +1,4 @@
+import { generateQR } from './../../utils/qr/index';
 import {
   createSession,
   getSessionById,
@@ -65,6 +66,26 @@ export default async function (fastify) {
       }
 
       reply.status(200).send({ message: 'Deleted' });
+    } catch (error) {
+      fastify.log.error(error);
+      reply.status(500).send({ error: 'Internal Server Error' });
+    }
+  });
+  fastify.get('/QRCode', async (request, reply) => {
+    try {
+      const { url } = request.headers;
+
+      if (!url) {
+        reply.status(400).send({ error: 'url not loyal' });
+        return;
+      }
+
+      const genQR = generateQR(url);
+
+      reply
+        .status(200)
+        .header('Content-Type', 'image/png')
+        .send(Buffer.from((await genQR).replace(/^data:image\/(png);base64,/, ''), 'base64'));
     } catch (error) {
       fastify.log.error(error);
       reply.status(500).send({ error: 'Internal Server Error' });

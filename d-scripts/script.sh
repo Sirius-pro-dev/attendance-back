d05deabe9daada5f5e408b97bab0d83ca49867ff
Daniel <dan.usov82@gmail.com>
2023-10-11 17:29:05 +0300

    fix: mongo port

--- d-scripts/script.sh
+++ d-scripts/script.sh
@@ -2,4 +2,4 @@
 docker stop sirius_mongo
 docker volume remove sirius_volume
 docker volume create sirius_volume
-docker run -e MONGO_INITDB_ROOT_USERNAME=test -e MONGO_INITDB_ROOT_PASSWORD=test -p 27017:27017 --name sirius_mongo -v sirius_volume:/data/db -d --rm mongo:4.4.23
+docker run -e MONGO_INITDB_ROOT_USERNAME=test -e MONGO_INITDB_ROOT_PASSWORD=test -p 27018:27018 --name sirius_mongo -v sirius_volume:/data/db -d --rm mongo:4.4.23

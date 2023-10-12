3b8c5a86d9325bcc0e933ffe2ee62014b21d75ca
Daniel Usov <dan.usov82@gmail.com>
2023-10-12 21:30:59 +0300

    fix: mongo port

--- src/configs/mongo/index.ts
+++ src/configs/mongo/index.ts
@@ -1,5 +1,5 @@
 export const mongoConfig = {
   connectURL: `mongodb://${process.env.SIRIUS_X_MONGO_USERNAME}:${process.env.SIRIUS_X_MONGO_PASSWORD}@${
     process.env.SIRIUS_X_DB
-  }:27018/${process.env.SIRIUS_X_MONGO_AUTHSOURCE ? `?authSource=${process.env.SIRIUS_X_MONGO_AUTHSOURCE}` : ''}`
+  }:27017/${process.env.SIRIUS_X_MONGO_AUTHSOURCE ? `?authSource=${process.env.SIRIUS_X_MONGO_AUTHSOURCE}` : ''}`
 };
--- src/connect.ts
+++ src/connect.ts
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 import { mongoConfig } from './configs/mongo';
 
 export const connect = async fastify => {
+  console.log(mongoConfig.connectURL);
+  
   await mongoose.connect(mongoConfig.connectURL);
 
   fastify.log.info('Connected to db');

fb511772a5b9b4cda987eb5e7ebf757cb3a6086c
Daniel Usov <dan.usov82@gmail.com>
2023-10-13 22:13:20 +0300

    feat: init work

--- src/configs/mongo/index.ts
+++ src/configs/mongo/index.ts
@@ -1,5 +1,7 @@
 export const mongoConfig = {
-  connectURL: `mongodb://${process.env.SIRIUS_X_MONGO_USERNAME}:${process.env.SIRIUS_X_MONGO_PASSWORD}@${
-    process.env.SIRIUS_X_DB
-  }:27017/${process.env.SIRIUS_X_MONGO_AUTHSOURCE ? `?authSource=${process.env.SIRIUS_X_MONGO_AUTHSOURCE}` : ''}`
+  connectURL: `mongodb://${process.env.SIRIUS_X_ATTENDANCE_MONGO_USERNAME}:${
+    process.env.SIRIUS_X_ATTENDANCE_MONGO_PASSWORD
+  }@${process.env.SIRIUS_X_DB}:${process.env.SIRIUS_X_ATTENDANCE_DB_PORT}/${
+    process.env.SIRIUS_X_MONGO_AUTHSOURCE ? `?authSource=${process.env.SIRIUS_X_MONGO_AUTHSOURCE}` : ''
+  }`
 };
